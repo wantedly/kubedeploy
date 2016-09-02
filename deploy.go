f11df7a98fc496af685aa0920efcd6c67cce043e
Yuto Furuta <mzq6mft9zz@gmail.com>
2016-09-02 18:14:11 +0900

    Add check running

--- deploy.go
+++ deploy.go
@@ -9,39 +9,61 @@ import (
 	"strings"
 	"time"
 
+	"github.com/fatih/color"
+
 	pb "gopkg.in/cheggaaa/pb.v1"
 
 	"k8s.io/kubernetes/pkg/api"
 	client "k8s.io/kubernetes/pkg/client/unversioned"
 )
 
-func isRunning(kubeClient *client.Client, pod string, namespace string) {
-	// get info
-	// status := getPodStatus(kubeClient, pod, namespace)
-	// check status
-
-	// return
+func isRunning(pod api.Pod) bool {
+	state := pod.Status.ContainerStatuses[0].State
+	if state.Running != nil {
+		return true
+	}
+	return false
 }
 
-func healthCheck(kubeClient *client.Client, targetPod api.Pod) bool {
+func checkRunning(kubeClient *client.Client, targetPod api.Pod, initialRunning bool) bool {
+
 	count := 60
 	bar := pb.StartNew(count)
 	for s := 0; s < count; s++ {
+		newPod := getTargetPod(kubeClient, targetPod.Name, targetPod.Namespace)
 		bar.Increment()
 		time.Sleep(time.Second)
-		if s%10 == 0 {
-			newTargetPod := getTargetPod(kubeClient, targetPod.Name, targetPod.Namespace)
-			state := newTargetPod.Status.ContainerStatuses[0].State
-			if state.Running == nil {
-				return false
-			}
+		if !isRunning(newPod) && initialRunning {
+			return false
 		}
 	}
 	return true
 }
 
-func replaceImage(podName, oldImage, newImage string) {
+func checkHealth(kubeClient *client.Client, targetPod api.Pod) bool {
 
+	// health check
+	if isRunning(targetPod) {
+		if !checkRunning(kubeClient, targetPod, true) {
+			return false
+		}
+	} else {
+		if !checkRunning(kubeClient, targetPod, false) {
+			return false
+		}
+	}
+
+	// last check
+	newPod := getTargetPod(kubeClient, targetPod.Name, targetPod.Namespace)
+	if !isRunning(newPod) {
+		return false
+	}
+
+	return true
+}
+
+func replaceImage(podName, oldImage, newImage string) {
+	printDeploy(oldImage, newImage)
 	commandOptions := []string{"get", "pod", podName, "-o", "yaml"}
 	result := execOutput("kubectl", commandOptions)
 	result = strings.Replace(result, oldImage, newImage, -1)
@@ -59,15 +81,13 @@ func replaceImage(podName, oldImage, newImage string) {
 func deploy(kubeClient *client.Client, params map[string]string) {
 
 	targetPod := getTargetPod(kubeClient, params["pod"], params["namespace"])
-	printDeploy(targetPod.Spec.Containers[0].Image, params["image"])
 	replaceImage(targetPod.Name, targetPod.Spec.Containers[0].Image, params["image"])
 
-	if healthCheck(kubeClient, targetPod) {
-		fmt.Println("Success!!")
+	if checkHealth(kubeClient, targetPod) {
+		color.Green("Success!!")
 	} else {
-		fmt.Println("Failed!!")
+		color.Red("Failed!!")
 		fmt.Println("Revert Start")
-		printDeploy(params["image"], targetPod.Spec.Containers[0].Image)
 		replaceImage(targetPod.Name, params["image"], targetPod.Spec.Containers[0].Image)
 		fmt.Println("Revert End")
 	}
