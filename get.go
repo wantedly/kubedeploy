49e7253bacde257b218bd5e07863ffba0c3076b9
Yuto Furuta <mzq6mft9zz@gmail.com>
2016-09-06 09:53:51 +0900

    List image tags

--- get.go
+++ get.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -23,8 +24,29 @@ func getTagList(image string) {
 	}
 	defer resp.Body.Close()
 
+	var f interface{}
 	byteArray, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(byteArray)) // htmlをstringで取得
+	err = json.Unmarshal(byteArray, &f)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	m := f.(map[string]interface{})
+	for _, v := range m {
+		switch vv := v.(type) {
+		case string:
+		case int:
+		case bool:
+		case []interface{}:
+			for i, u := range vv {
+				id := u.(map[string]interface{})["docker_image_id"].(string)
+				fmt.Println(i+1, u.(map[string]interface{})["name"].(string)+"_"+id)
+			}
+		default:
+		}
+	}
+
 }
 
 func getTargetPod(kubeClient *client.Client, podName string, namespace string) api.Pod {
