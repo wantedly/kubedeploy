c0d98fe52e2a7ae4611db307f6ce2ac46310c83a
Yuto Furuta <mzq6mft9zz@gmail.com>
2016-09-07 14:28:41 +0900

    Fix help

--- help.go
+++ help.go
@@ -7,7 +7,7 @@ func help() {
 Usage:
 $ kubedeploy get [-n namespace]
 $ kubedeploy replace -p pod -i image -n namespace
-$ kubedeploy deploy -s service
+$ kubedeploy deploy -s service -n namespace
 $ kubedeploy list -i image
 	`)
 }
