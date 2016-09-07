b2fb5d96bf4bfa7c10fd1713dac173d3b4fb3d3a
Yuto Furuta <mzq6mft9zz@gmail.com>
2016-09-07 14:54:10 +0900

    Change help ui

--- help.go
+++ help.go
@@ -1,13 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/olekukonko/tablewriter"
+)
+
+func printHelp(commands []map[string]string) {
+
+	fmt.Println("=== help === ")
+
+	data := [][]string{}
+	for _, command := range commands {
+		data = append(data, []string{
+			command["command"],
+			command["option"],
+			command["description"],
+		})
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{
+		"command",
+		"option",
+		"description",
+	})
+	table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+	table.SetCenterSeparator("|")
+	table.AppendBulk(data)
+	table.Render()
+	fmt.Println()
+}
 
 func help() {
-	fmt.Println(`
-Usage:
-$ kubedeploy get [-n namespace]
-$ kubedeploy replace -p pod -i image -n namespace
-$ kubedeploy deploy -s service -n namespace
-$ kubedeploy list -i image
-	`)
+	commands := []map[string]string{
+		map[string]string{
+			"command":     "kubedeploy get",
+			"option":      "[-n namespace]",
+			"description": "Get Pods and Services information.",
+		},
+		map[string]string{
+			"command":     "kubedeploy list",
+			"option":      "-i image",
+			"description": "List image tags",
+		},
+		map[string]string{
+			"command":     "kubedeploy replace",
+			"option":      "-p pod -i newImage -n namespace",
+			"description": "Replace new image from old one in pod ",
+		},
+		map[string]string{
+			"command":     "kubedeploy deploy",
+			"option":      "-s service",
+			"description": "Blue-Green Deploy",
+		},
+		map[string]string{
+			"command":     "kubedeploy help",
+			"option":      "",
+			"description": "Print Usage",
+		},
+	}
+
+	printHelp(commands)
+
 }
